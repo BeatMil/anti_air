dd81eac33b603178b687ff60d607964a470f8316
beatmil <anutorn@gmail.com>
2022-05-13 14:17:39 +0700

    initial commit

---
+++ script/client_node.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+func _ready():
+	var network = NetworkedMultiplayerENet.new()
+	network.create_client("127.0.0.1", 1998)
+	get_tree().set_network_peer(network)
+	network.connect("connection_failed",self,"_on_connection_failed")
+	get_tree().multiplayer.connect("network_peer_packet",self,"_on_packet_received")
+
+  
+func _on_connection_failed(error):
+	$status_label.text = "Error connecting to server " + error
+
+
+func _on_packet_received(id,packet):
+	$labelServerData.text = packet.get_string_from_ascii()
+
+
+func _on_connect_button_pressed():
+	var network = NetworkedMultiplayerENet.new()
+	network.create_client("127.0.0.1", 1998)
+	get_tree().set_network_peer(network)
+	$status_label.text = "Connect!"
+	
+
+
+func _on_disconnect_button_pressed():
+	get_tree().set_network_peer(null)
+	$status_label.text = "Disconnect!"
